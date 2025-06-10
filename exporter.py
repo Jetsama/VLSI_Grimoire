63e28ad121d4f882b486d81cfbe8a9a8bc033f7a
jetsama <jonathan.e.hess@gmail.com>
2025-06-10 16:08:35 +0000

    Remove html page

---
+++ exporter.py
@@ -0,0 +1,15 @@
+from marimo import MarimoIslandGenerator
+
+# Create the generator from file
+generator = MarimoIslandGenerator.from_file("plot_test.py", display_code=True)
+
+# Generate and print the HTML without building
+# This will still work for basic rendering, though without running the cells
+# html = generator.render_html(include_init_island=False)
+html = generator.render_html(include_init_island=True)
+
+print(html)
+# Save the HTML to a file
+output_file = "output.html"
+with open(output_file, "w", encoding="utf-8") as f:
+    f.write(html)
